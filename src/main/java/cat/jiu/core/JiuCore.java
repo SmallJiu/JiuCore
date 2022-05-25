9108da155c02e62cef589df8becc710f445c3317
small_jiu <70803028+SmallJiu@users.noreply.github.com>
2022-05-25 23:32:35 +0800

    1.0.10-fix-1

--- src/main/java/cat/jiu/core/JiuCore.java
+++ src/main/java/cat/jiu/core/JiuCore.java
@@ -48,7 +48,7 @@ public class JiuCore implements IInFluidCraftingEvent {
 	public static final String MODID = "jiucore";
 	public static final String NAME = "JiuCore";
 	public static final String OWNER = "small_jiu";
-	public static final String VERSION = "1.0.10-20220522225957";
+	public static final String VERSION = "1.0.10-20220525233201";
 	
 	private static byte isTest = -1; // if is IDE, you can set to '1' to enable some test stuff
 	public static final boolean test() {
